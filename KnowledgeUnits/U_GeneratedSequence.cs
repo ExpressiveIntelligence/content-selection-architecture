1330b3efbce1756e5d13b31118749811ee8e1761
michael <michaelm@soe.ucsc.edu>
2019-05-13 13:26:16 +0100

    Marked U_GeneratedSequence as deprecated.

--- KnowledgeUnits/U_GeneratedSequence.cs
+++ KnowledgeUnits/U_GeneratedSequence.cs
@@ -1,7 +1,12 @@
-﻿using CSA.Core;
+﻿using System;
+using CSA.Core;
 
 namespace CSA.KnowledgeUnits
 {
+    /*
+     * U_GeneratedSequence is deprecated. Add a KC_Sequence KnowledgeComponent to a Unit for the same functionality. 
+     */
+    [Obsolete("U_GeneratedSequence is deprecated. Add a KC_Sequence KnoweldgeComponent to a Unit for the same functionality.")]
     public class U_GeneratedSequence : Unit
     {
         public IUnit[] Sequence { get; }
