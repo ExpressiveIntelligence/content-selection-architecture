646b5f2f49f320588d81f58b5c7909421adc0df5
michael <michaelm@soe.ucsc.edu>
2019-06-06 15:48:59 +0100

    Revert "Removed IChoicePresenter_Old."
    This reverts commit 3306217aa7b29d20780d11069d1f9595b7aebb79.

---
+++ KnowledgeSources/IChoicePresenter_Old.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CSA.Core;
+
+namespace CSA.KnowledgeSources
+{
+    /*
+     * Shared interface for both reactive and scheduled choice presenters.
+     */
+    [Obsolete("Use KnowledgeComponent-based version of this interface.")]
+    public interface IChoicePresenter_Old
+    {
+        void SelectChoice(ContentUnit[] choices, uint choiceMade);
+    }
+}
